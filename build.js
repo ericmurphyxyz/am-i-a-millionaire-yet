0e308f2ccd93fd7348c1165f5ace1f250f217fe1
Eric Murphy <eric@ericmurphy.xyz>
2019-06-26 21:54:10 +0700

    Fixed build script

--- build.js
+++ build.js
@@ -1,33 +1,63 @@
 const fs = require('fs');
+const http = require('http');
 
-const fixerUrl = `https://data.fixer.io/api/latest?access_key=${process.env.FIXER_API_KEY}&format=1`;
-
-const {rates, date} = require(fixerUrl);
+// get countries from file
 const {results: countries} = require('./countries.json');
 
-const newObject = {};
-const usdRate = rates.USD;
-newObject.date = date;
-newObject.currencies = [];
+// get rates from fixer
+http
+  .get(
+    `http://data.fixer.io/api/latest?access_key=${process.env.FIXER_API_KEY}&format=1`,
+    response => {
+      let data = '';
 
-for (const country in countries) {
-  // destructure variables from each country
-  const {currencyId, currencyName, currencySymbol, name, emoji} = countries[
-    country
-  ];
+      response.on('data', chunk => {
+        data += chunk;
+      });
 
-  // convert rates to USD
-  const rate = rates[currencyId] / usdRate;
-  // make an object for each country
-  const countryObject = {
-    name,
-    rate,
-    currencyId,
-    currencyName,
-    currencySymbol,
-    emoji,
-  };
-  newObject.currencies.push(countryObject);
-}
+      response.on('end', () => {
+        const json = JSON.parse(data);
 
-fs.writeFileSync('list.json', JSON.stringify(newObject), {encoding: 'utf8'});
+        // get rates
+        const rates = json.rates;
+        const date = json.date;
+
+        const usdRate = rates.USD;
+
+        const newObject = {};
+        newObject.date = date;
+        newObject.currencies = [];
+
+        for (const country in countries) {
+          // destructure variables from each country
+          const {
+            currencyId,
+            currencyName,
+            currencySymbol,
+            name,
+            emoji,
+          } = countries[country];
+
+          // convert rates to USD
+          const rate = rates[currencyId] / usdRate;
+          // make an object for each country
+          const countryObject = {
+            name,
+            rate,
+            currencyId,
+            currencyName,
+            currencySymbol,
+            emoji,
+          };
+          newObject.currencies.push(countryObject);
+        }
+
+        fs.writeFileSync('list.json', JSON.stringify(newObject), {
+          encoding: 'utf8',
+        });
+      });
+    },
+  )
+  .on('error', error => {
+    console.log(`Error: ${error.message}`);
+  });
