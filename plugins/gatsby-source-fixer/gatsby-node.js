c6a1904cf85479ac78dd5fb755e877af5d7cc8e4
Eric Murphy <ericnmurphy@gmail.com>
2019-10-20 22:32:05 +0700

    Converted Fixer API to Gatsby nodes

--- plugins/gatsby-source-fixer/gatsby-node.js
+++ plugins/gatsby-source-fixer/gatsby-node.js
@@ -2,30 +2,49 @@ const fetch = require("node-fetch")
 const queryString = require("query-string")
 
 exports.sourceNodes = (
-    { actions, createNodeId, createContentDigest },
-    configOptions
+  { actions, createNodeId, createContentDigest },
+  configOptions
 ) => {
-    const { createNode } = actions
+  const { createNode } = actions
 
-    // Gatsby adds a configOption that's not needed for this plugin, delete it
-    delete configOptions.plugins
+  // Gatsby adds a configOption that's not needed for this plugin, delete it
+  delete configOptions.plugins
 
-    // Convert the options object into a query string
-    const apiOptions = queryString.stringify(configOptions)
-    // Join apiOptions with the Pixabay API URL
-    const apiUrl = `http://data.fixer.io/api/latest?${apiOptions}`
-    // Gatsby expects sourceNodes to return a promise
-    return (
-        // Fetch a response from the apiUrl
-        fetch(apiUrl)
-            // Parse the response as JSON
-            .then(response => response.json())
-            // Process the JSON data into a node
-            .then(({ rates }) => {
-                // For each query result
-                Object.keys(rates).forEach(rate => {
-                    console.log(`${rate} rate is:`, rates[rate])
-                })
-            })
-    )
+  // Helper function that processes currency/rate pairs to match Gatsby's node structure
+  const processRate = (currency, rate, index) => {
+    const node = { currency, rate }
+    const nodeId = createNodeId(`fixer-rate-${index}`)
+    const nodeContent = JSON.stringify(node)
+    const nodeData = Object.assign({}, node, {
+      id: nodeId,
+      parent: null,
+      children: [],
+      internal: {
+        type: `FixerRate`,
+        content: nodeContent,
+        contentDigest: createContentDigest(node),
+      },
+    })
+    return nodeData
+  }
+
+  // Convert the options object into a query string
+  const apiOptions = queryString.stringify(configOptions)
+  // Join apiOptions with the Pixabay API URL
+  const apiUrl = `http://data.fixer.io/api/latest?${apiOptions}`
+  // Gatsby expects sourceNodes to return a promise
+  return (
+    // Fetch a response from the apiUrl
+    fetch(apiUrl)
+      // Parse the response as JSON
+      .then(response => response.json())
+      .then(({ rates }) => {
+        Object.keys(rates).forEach((currency, index) => {
+          // Covert each currency/rate pair to a Gatsby Node
+          const nodeData = processRate(currency, rates[currency], index)
+          // Use Gatsby's createNode helper to create a node from the node data
+          createNode(nodeData)
+        })
+      })
+  )
 }
