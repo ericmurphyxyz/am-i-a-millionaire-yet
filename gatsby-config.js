66a4ddfc4bf19c85824128c2b7523cb480419836
Eric Murphy <ericnmurphy@gmail.com>
2019-10-31 19:38:11 +0800

    Updating favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,13 +20,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Am I a Millionaire Yet?`,
+        short_name: `Millionaire`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        display: `standalone`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
   ],
