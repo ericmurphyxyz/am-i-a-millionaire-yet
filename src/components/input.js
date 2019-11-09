7f9c67a83a57728b1da4af030597f00447c34aa3
Eric Murphy <ericnmurphy@gmail.com>
2019-11-09 16:35:42 +0700

    Added more styling to form

--- src/components/input.js
+++ src/components/input.js
@@ -4,6 +4,51 @@ import styled from "styled-components"
 const InputLabel = styled.label`
   display: block;
   font-weight: bold;
+  margin-bottom: 5px;
+`
+
+const InputWrapper = styled.div`
+  display: flex;
+`
+
+const InputPrepend = styled.div`
+  display: flex;
+  margin-right: -1px;
+
+  span {
+    display: flex;
+    align-items: center;
+    padding: 0.15rem 0.5rem;
+    background-color: #e9ecef;
+    border: 1px solid #ced4da;
+    border-radius: 0.25rem;
+    border-top-right-radius: 0;
+    border-bottom-right-radius: 0;
+  }
+`
+
+const NumberInput = styled.input`
+  padding: 0.15rem 0.375rem;
+  margin-right: 10px;
+  border: 1px solid #ced4da;
+  border-radius: 0.25rem;
+  border-top-left-radius: 0;
+  border-bottom-left-radius: 0;
+`
+
+const Button = styled.button`
+  padding: 0.15rem 0.75rem;
+  color: #fff;
+  background-color: #6c757d;
+  border: 1px solid #6c757d;
+  border-radius: 0.25rem;
+  cursor: pointer;
+  transition: background-color 0.15s ease-in-out, border-color 0.15s ease-in-out;
+
+  &:hover {
+    background-color: #5a6268;
+    border-color: #5a6268;
+  }
 `
 
 class Input extends Component {
@@ -12,13 +57,18 @@ class Input extends Component {
     return (
       <form onSubmit={handleSubmit}>
         <InputLabel htmlFor="net-worth">What's your net worth?</InputLabel>
-        <input
-          type="number"
-          id="net-worth"
-          value={inputValue}
-          onChange={handleInputChange}
-        />
-        <button type="submit">Calculate</button>
+        <InputWrapper>
+          <InputPrepend>
+            <span>$</span>
+          </InputPrepend>
+          <NumberInput
+            type="number"
+            id="net-worth"
+            value={inputValue}
+            onChange={handleInputChange}
+          />
+          <Button type="submit">Calculate</Button>
+        </InputWrapper>
       </form>
     )
   }
